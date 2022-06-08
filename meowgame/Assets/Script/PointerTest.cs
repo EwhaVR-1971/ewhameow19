220c8e8892b832d6da8e26ccf430bc9d788c3b50
s0ooo0k <izz_m2@naver.com>
2022-06-08 21:34:28 +0900

    [refact] size

---
+++ meowgame/Assets/Script/PointerTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class PointerTester : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler 
+{
+
+// Use this for initialization
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Pointer is Down");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Pointer is Up");
+    }
+}
