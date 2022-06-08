fce6ab0ccc058478a2ddbefcfe7f57919f0a0b06
Heewon Lee <heewon223@gmail.com>
2022-06-08 14:21:36 +0900

    [add] car crash with cat
    car crash animation added

---
+++ meowgame/Assets/Script/MovingCar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCar : MonoBehaviour
+{
+    public float timer;
+    public int waitingTime;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer < waitingTime)
+        {
+            //Action
+            transform.position += new Vector3(0, 0, 0.5f);
+        }
+    }
+}
---
+++ meowgame/Assets/Script/MovingCat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCat : MonoBehaviour
+{
+    public float timer;
+    public int waitingTime;
+    
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer < waitingTime)
+        {
+            //Action
+            transform.position += new Vector3(0.1f, 0, 0);
+        }
+    }
+}
